3fe7ec7418b45df41387f961f27c2ef4fe07462d
mykhailo.skliar <mykhailo.skliar@exasol.com>
2026-04-22 13:47:47 +0200

    Refactor/3: Move ansible related code and tests from ai-lab to ansible-runner-wrapper

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 # imports all nox task provided by the toolbox
-from exasol.toolbox.nox.tasks import *
+from exasol.toolbox.nox.tasks import *  # noqa: F403
 
 # default actions to be run if nothing is explicitly specified with the -s option
 nox.options.sessions = ["format:fix"]
