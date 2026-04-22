fb7a6f418d35115bf0fd9c361e848379df8244a9
ckunki <christoph.kuhnke@exasol.com>
2026-04-22 17:30:42 +0200

    reverted formatting

--- test/unit/test_ansible.py
+++ test/unit/test_ansible.py
@@ -32,20 +32,17 @@ from exasol.ds.sandbox.lib.setup_ec2.run_install_dependencies import (
 
 class AnsibleTestAccess:
 
-    def __init__(
-        self, delegate: Optional[Callable[[str, AnsibleRunContext], None]] = None
-    ):
+    def __init__(self, delegate: Optional[Callable[[str, AnsibleRunContext], None]] = None):
         self.call_arguments = None
         self.arguments = namedtuple("Arguments", "private_data_dir run_ctx")
         self.delegate = delegate
 
-    def run(
-        self,
-        private_data_dir: str,
-        run_ctx: AnsibleRunContext,
-        event_handler: Callable[[AnsibleEvent], bool],
-        event_logger: Callable[[str], None],
-    ):
+    def run(self,
+            private_data_dir: str,
+            run_ctx: AnsibleRunContext,
+            event_handler: Callable[[AnsibleEvent], bool],
+            event_logger: Callable[[str], None],
+            ):
         self.call_arguments = self.arguments(private_data_dir, run_ctx)
         if self.delegate is not None:
             self.delegate(private_data_dir, run_ctx)
@@ -54,7 +51,7 @@ class AnsibleTestAccess:
 def _extra_vars(config):
     return {
         "ansible_runner_wrapper_version": config.ansible_runner_wrapper_version,
-        "work_in_progress_notebooks": False,
+        'work_in_progress_notebooks': False,
     }
 
 
@@ -71,23 +68,6 @@ def _run_context(
     else:
         extra_vars = extra_vars or {}
     return AnsibleRunContext(playbook=other.playbook, extra_vars=extra_vars)
-
-
-@pytest.mark.skip
-def test_old_run_ansible_default_values(test_config: ConfigObject):
-    """
-    Test which executes run_install_dependencies with default values
-    (default playbook and default ansible variables)
-    """
-
-    ansible_access = AnsibleTestAccess()
-    run_install_dependencies(ansible_access, test_config)
-    expected_ansible_run_context = AnsibleRunContext(
-        playbook="ec2_playbook.yml",
-        extra_vars=_extra_vars(test_config),
-    )
-    assert ansible_access.call_arguments.private_data_dir.startswith("/tmp")
-    assert ansible_access.call_arguments.run_ctx == expected_ansible_run_context
 
 
 def test_run_ansible_default_values(test_config: ConfigObject):
@@ -109,19 +89,11 @@ def test_run_ansible_custom_playbook(test_config):
     Test which executes run_install_dependencies with default ansible variable, but a custom playbook
     """
     ansible_access = AnsibleTestAccess()
-    ansible_run_context = AnsibleRunContext(
-        playbook="my_playbook.yml", extra_vars=dict()
-    )
-    run_install_dependencies(
-        ansible_access,
-        test_config,
-        host_infos=tuple(),
-        ansible_run_context=ansible_run_context,
-    )
+    ansible_run_context = AnsibleRunContext(playbook="my_playbook.yml", extra_vars=dict())
+    run_install_dependencies(ansible_access, test_config, host_infos=tuple(), ansible_run_context=ansible_run_context)
 
     expected_ansible_run_context = AnsibleRunContext(
-        playbook="my_playbook.yml", extra_vars=_extra_vars(test_config)
-    )
+        playbook="my_playbook.yml", extra_vars=_extra_vars(test_config))
     assert ansible_access.call_arguments.private_data_dir.startswith("/tmp")
     assert ansible_access.call_arguments.run_ctx == expected_ansible_run_context
 
@@ -131,20 +103,13 @@ def test_run_ansible_custom_variables(test_config):
     Test which executes run_install_dependencies with custam playbook and custom ansible variables
     """
     ansible_access = AnsibleTestAccess()
-    ansible_run_context = AnsibleRunContext(
-        playbook="my_playbook.yml", extra_vars={"my_var": True}
-    )
-    run_install_dependencies(
-        ansible_access,
-        test_config,
-        host_infos=tuple(),
-        ansible_run_context=ansible_run_context,
-    )
+    ansible_run_context = AnsibleRunContext(playbook="my_playbook.yml", extra_vars={"my_var": True})
+    run_install_dependencies(ansible_access, test_config, host_infos=tuple(),
+                             ansible_run_context=ansible_run_context)
     extra_vars = _extra_vars(test_config)
     extra_vars.update({"my_var": True})
     expected_ansible_run_context = AnsibleRunContext(
-        playbook="my_playbook.yml", extra_vars=extra_vars
-    )
+        playbook="my_playbook.yml", extra_vars=extra_vars)
     assert ansible_access.call_arguments.private_data_dir.startswith("/tmp")
     assert ansible_access.call_arguments.run_ctx == expected_ansible_run_context
 
@@ -153,9 +118,7 @@ def test_run_ansible_check_inventory_empty_host(test_config):
     empty_inventory = "[ec2]\n\n"
 
     def check_inventory(work_dir: str, ansible_run_context: AnsibleRunContext):
-        with open(
-            f"{work_dir}/inventory",
-        ) as f:
+        with open(f"{work_dir}/inventory", ) as f:
             inventory_content = f.read()
         assert inventory_content == empty_inventory
 
@@ -166,17 +129,12 @@ def test_run_ansible_check_inventory_custom_host(test_config):
     custom_inventory = "[ec2]\n\nmy_host ansible_ssh_private_key_file=my_key\n\n"
 
     def check_inventory(work_dir: str, ansible_run_context: AnsibleRunContext):
-        with open(
-            f"{work_dir}/inventory",
-        ) as f:
+        with open(f"{work_dir}/inventory", ) as f:
             inventory_content = f.read()
         assert inventory_content == custom_inventory
 
-    run_install_dependencies(
-        AnsibleTestAccess(check_inventory),
-        test_config,
-        host_infos=(HostInfo("my_host", "my_key"),),
-    )
+    run_install_dependencies(AnsibleTestAccess(check_inventory), test_config,
+                             host_infos=(HostInfo("my_host", "my_key"),))
 
 
 def test_run_ansible_check_default_repository(test_config):
@@ -208,31 +166,18 @@ def test_run_ansible_check_multiple_repositories(test_config):
         p = pathlib.Path(f"{work_dir}/ansible_sample_playbook.yml")
         assert p.exists()
 
-    test_repositories = default_repositories + (
-        AnsibleResourceRepository(test.ansible),
-    )
-    run_install_dependencies(
-        AnsibleTestAccess(check_playbooks),
-        test_config,
-        host_infos=tuple(),
-        ansible_run_context=default_ansible_run_context,
-        ansible_repositories=test_repositories,
-    )
+    test_repositories = default_repositories + (AnsibleResourceRepository(test.ansible),)
+    run_install_dependencies(AnsibleTestAccess(check_playbooks), test_config, host_infos=tuple(),
+                             ansible_run_context=default_ansible_run_context, ansible_repositories=test_repositories)
 
 
-def test_run_ansible_check_multiple_repositories_with_same_content_causes_exception(
-    test_config,
-):
+def test_run_ansible_check_multiple_repositories_with_same_content_causes_exception(test_config):
     """
     Test that multiple repositories containing same files raises an runtime exception.
     """
     conflict = AnsibleResourceRepository(test.unit.resources.ansible_conflict)
     test_repositories = default_repositories + (conflict,)
     with pytest.raises(RuntimeError):
-        run_install_dependencies(
-            AnsibleTestAccess(),
-            test_config,
-            host_infos=tuple(),
-            ansible_run_context=default_ansible_run_context,
-            ansible_repositories=test_repositories,
-        )
+        run_install_dependencies(AnsibleTestAccess(), test_config, host_infos=tuple(),
+                                 ansible_run_context=default_ansible_run_context,
+                                 ansible_repositories=test_repositories)
