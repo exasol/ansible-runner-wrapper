bc0126696dd99c28738aa748068f25245adf173a
Christoph Pirkl <christoph.pirkl@exasol.com>
2026-07-08 15:24:09 +0200

    Fail early when docker is not available for integration tests

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -1,17 +1,35 @@
 import io
 import logging
+import os
 from collections.abc import Iterable
 from inspect import cleandoc
 
 import docker
 import pytest
+from docker.errors import DockerException
 from docker.models.containers import Container as DockerContainer
 
 LOG = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="session")
-def arw_itest_docker_image() -> str:
+def arw_itest_docker_client() -> docker.DockerClient:
+    try:
+        client = docker.from_env()
+        client.ping()
+    except (DockerException, OSError, PermissionError) as exception:
+        docker_host = os.environ.get("DOCKER_HOST", "<unset>")
+        pytest.fail(
+            "Integration tests require Docker access. "
+            f"Could not connect to the Docker daemon via DOCKER_HOST={docker_host!r}: {exception}. "
+            "If Docker is available on this machine, rerun the tests in an environment that allows access "
+            "to the Docker socket."
+        )
+    return client
+
+
+@pytest.fixture(scope="session")
+def arw_itest_docker_image(arw_itest_docker_client: docker.DockerClient) -> str:
     image_name = "arw/itest:latest"
     LOG.info("Creating Docker image %s", image_name)
     install_args = "--no-install-recommends --assume-yes python3 python3-pexpect"
@@ -21,13 +39,15 @@ def arw_itest_docker_image() -> str:
         RUN apt-get update && apt-get install {install_args}
         """)
     stream = io.BytesIO(docker_file_content.encode("utf-8"))
-    client = docker.from_env()
-    client.images.build(fileobj=stream, tag=image_name, rm=True)
+    arw_itest_docker_client.images.build(fileobj=stream, tag=image_name, rm=True)
     return image_name
 
 
 @pytest.fixture(scope="session")
-def arw_itest_docker_container(arw_itest_docker_image) -> Iterable[DockerContainer]:
+def arw_itest_docker_container(
+    arw_itest_docker_client: docker.DockerClient,
+    arw_itest_docker_image: str,
+) -> Iterable[DockerContainer]:
     """
     Run a docker container.
 
@@ -35,13 +55,12 @@ def arw_itest_docker_container(arw_itest_docker_image) -> Iterable[DockerContain
     ansible in integration tests.
     """
 
-    client = docker.from_env()
     name = "ARW_ITEST"
     image = arw_itest_docker_image
     container = None
     try:
         LOG.info("Starting Docker container %s of image %s", name, image)
-        container = client.containers.create(
+        container = arw_itest_docker_client.containers.create(
             image=image,
             name=name,
             command="sleep infinity",
