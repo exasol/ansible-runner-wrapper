4c8e8f5416daa0dfebf8e695f1bf233afe0f1d0d
Christoph Pirkl <christoph.pirkl@exasol.com>
2026-07-08 15:51:09 +0200

    Define tested python versions

--- noxconfig.py
+++ noxconfig.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from exasol.toolbox.config import BaseConfig
 
 PROJECT_CONFIG = BaseConfig(
-    project_name="ansible",
+    project_name="ansible-runner-wrapper",
     root_path=Path(__file__).parent,
+    python_versions=("3.12", "3.13", "3.14"),
 )
