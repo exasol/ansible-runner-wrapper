7966564671c5c3d08f50e21f943196c5801f3de2
Christoph Pirkl <christoph.pirkl@exasol.com>
2026-07-08 16:26:00 +0200

    Fix linting

--- noxconfig.py
+++ noxconfig.py
@@ -2,9 +2,19 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from pydantic import computed_field
+
 from exasol.toolbox.config import BaseConfig
 
-PROJECT_CONFIG = BaseConfig(
+
+class ProjectConfig(BaseConfig):
+    @computed_field  # type: ignore[misc]
+    @property
+    def source_code_path(self) -> Path:
+        return self.root_path / "exasol" / "ansible"
+
+
+PROJECT_CONFIG = ProjectConfig(
     project_name="ansible-runner-wrapper",
     root_path=Path(__file__).parent,
     python_versions=("3.12", "3.13", "3.14"),
